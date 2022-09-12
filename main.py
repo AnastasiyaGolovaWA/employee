f79465a467e917f2398993ac67c778e079ec2418
Anastasiya.Golova <anastasiya.golova@cellpointmobile.com>
2022-09-12 22:11:14 +0400

    test database connection

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
+from os import environ
+
+import databases
 from fastapi import FastAPI
+from sqlalchemy import select
+
+from models.employee import employees_table
+
+DB_USER = environ.get("DB_USER", "postgres")
+DB_PASSWORD = environ.get("DB_PASSWORD", "root")
+DB_HOST = environ.get("DB_HOST", "localhost")
+DB_NAME = "employees"
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+)
+
+database = databases.Database(SQLALCHEMY_DATABASE_URL)
 
 app = FastAPI()
 
 
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+async def read_root():
+    query = (
+        select(
+            [
+                employees_table.c.id,
+                employees_table.c.email,
+                employees_table.c.firstName,
+                employees_table.c.lastName,
+                employees_table.c.experience,
+            ]
+        )
+    )
+    return await database.fetch_all(query)
