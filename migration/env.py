4d1c128d3f6078a506b07bfdea473e73c33ad65d
Anastasiya.Golova <anastasiya.golova@cellpointmobile.com>
2022-09-12 21:36:42 +0400

    create table in database

--- migration/env.py
+++ migration/env.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,20 +7,30 @@ from sqlalchemy import pool
 
 from alembic import context
 
+# add current path to PYTHONPATH, otherwise app module will not be found when alembic executing
+sys.path.append(os.getcwd())
+
+from models import database, employee
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
+section = config.config_ini_section
+config.set_section_option(section, "DB_USER", database.DB_USER)
+config.set_section_option(section, "DB_PASS", database.DB_PASSWORD)
+config.set_section_option(section, "DB_NAME", database.DB_NAME)
+config.set_section_option(section, "DB_HOST", database.DB_HOST)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = [employee.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -26,7 +38,7 @@ target_metadata = None
 # ... etc.
 
 
-def run_migrations_offline() -> None:
+def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -50,7 +62,7 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
+def run_migrations_online():
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
