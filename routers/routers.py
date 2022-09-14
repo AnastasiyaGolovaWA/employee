148968195c55e907dd33df68c9cf851f39841655
Anastasiya.Golova <anastasiya.golova@cellpointmobile.com>
2022-09-14 16:26:39 +0400

    add route get

--- routers/routers.py
+++ routers/routers.py
@@ -1,9 +1,11 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from database.database import SessionLocal, engine
 from models import employee
-from schemas.schemas import EmployeeDetailsModel
+from schemas.schemas import EmployeeDetailsModel, EmployeeBase
 from utils import crud
 
 employee.Base.metadata.create_all(bind=engine)
@@ -20,7 +22,15 @@ def get_session():
 
 @router.get("/employees/{firstName}", response_model=EmployeeDetailsModel)
 def find_employee_by_firstname(firstName: str, session: Session = Depends(get_session)):
-    item = crud.get_employee_by_firstname(session=session, firstName=firstName)
-    if item is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return item.serialize
+    employee = crud.get_employee_by_firstname(session=session, firstName=firstName)
+    if employee is None:
+        raise HTTPException(status_code=404, detail="employee not found")
+    return employee.serialize
+
+
+@router.get("/employees/", response_model=List[EmployeeDetailsModel])
+def get_employees(
+        skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+):
+    employees = crud.get_employees(session=session, skip=skip, limit=limit)
+    return [i.serialize for i in employees]
--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 
 from models.employee import Employee
@@ -5,3 +7,7 @@ from models.employee import Employee
 
 def get_employee_by_firstname(session: Session, firstName: str) -> Employee:
     return session.query(Employee).filter(Employee.firstName == firstName).first()
+
+
+def get_employees(session: Session, skip: int = 0, limit: int = 100) -> List[Employee]:
+    return session.query(Employee).offset(skip).limit(limit).all()
