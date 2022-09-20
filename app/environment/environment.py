1edc4ceb1be512a276c58496c529add700a9e46f
Anastasiya.Golova <anastasiya.golova@cellpointmobile.com>
2022-09-20 21:46:34 +0400

    fix db port

--- app/environment/environment.py
+++ app/environment/environment.py
@@ -4,6 +4,6 @@ DB_USER = environ.get("DB_USER", "postgres")
 DB_PASSWORD = environ.get("DB_PASSWORD", "root")
 DB_HOST = environ.get("DB_HOST", "127.0.0.1")
 DB_NAME = environ.get("DB_NAME", "employees")
-DB_PORT = environ.get("DB_PORT", "5437")
+DB_PORT = environ.get("DB_PORT", "5432")
 DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 print(DB_URL)
